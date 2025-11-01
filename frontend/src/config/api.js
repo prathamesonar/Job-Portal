dc4a5d50e0fa3672ba9a74dd88e152dba99b54cc
Prathamesh Sonar <pratham25092004@gmail.com>
2025-11-01 21:34:52 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,12 +1,7 @@
-// File: frontend/src/config/api.js - REVISED
 
-const isProduction = import.meta.env.PROD;
-
-// Read from environment variable (Vercel needs this set)
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL; 
 
 const API_BASE_URL = isProduction 
-  ? `${BACKEND_URL}/api` // This will use the value set in Vercel during build
-  : 'http://localhost:5000/api';  // Local Development URL
+  ? `${BACKEND_URL}/api` 
+  : 'http://localhost:5000/api'; 
 
 export default API_BASE_URL;
