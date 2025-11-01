1a704f63fde3a62775d2c32da23c33e9ac2d562b
Prathamesh Sonar <pratham25092004@gmail.com>
2025-11-01 21:19:30 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -3,7 +3,7 @@
 const isProduction = import.meta.env.PROD;
 
 const API_BASE_URL = isProduction 
-  ? 'YOUR_RENDER_BACKEND_URL/api' // Production URL 
+  ? 'https://jobc-8hsk.onrender.com/api' // Production URL 
   : 'http://localhost:5000/api';  // Local Development URL
 
 export default API_BASE_URL;
